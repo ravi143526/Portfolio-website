92287d587e56e87aa6ada9670c82c8983ffb3909
ravi143526 <ravikumarcse123@gmail.com>
2024-04-29 17:53:05 +0530

    Build errors fixed

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -15,11 +15,11 @@ const EmailSection = () => {
       id="contact"
     >
       <div>
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm currently looking for new opportunities, my inbox is always open,
-          whether you have a question or just want to say hi, I'll try my best
+          I&apos;m currently looking for new opportunities, my inbox is always open,
+          whether you have a question or just want to say hi, I&apos;ll try my best
           to get back to you!
         </p>
         <div className="socials flex fle3x-ow gap-2">
